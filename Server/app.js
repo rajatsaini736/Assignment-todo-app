fecda5e9061b5653ecdbd02e6095cdae1a2ad930
rajatsaini736 <rajatsaini736@gmail.com>
2021-11-21 17:06:52 +0530

    app config changes

--- Server/app.js
+++ Server/app.js
@@ -21,9 +21,9 @@ app.use('/', parentTaskRouter);
 app.use('/subtask', subTaskRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
